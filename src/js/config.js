c5f24bb5701d7a8d7fbacbe6a61921eb0c4b5516
pav_nk <pav_nk@vk.com>
2025-02-24 17:11:52 +0300

    feat: updating the interface and getting data from the server via the API

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+const API_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`;
+
+export { API_KEY, API_URL };
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,49 @@
-import "../scss/style.scss";
+import '../scss/style.scss';
+import { API_KEY, API_URL } from './config';
+
+import windImg from '../images/wind.svg';
+import rainImg from '../images/rain.svg';
+import snowImg from '../images/snow.svg';
+import clearDayImg from '../images/clear-day.svg';
+import clearNightImg from '../images/clear-night.svg';
+import cloudyImg from '../images/cloudy.svg';
+import fogImg from '../images/fog.svg';
+import partlyCloudyDayImg from '../images/partly-cloudy-day.svg';
+import partlyCloudyNightImg from '../images/partly-cloudy-night.svg';
+
+const weatherImg = document.querySelector('#weather-img');
+const preloader = document.querySelector('#preloader');
+const searchForm = document.getElementById('search-form');
+const cardWrapper = document.getElementById('card-wrapper');
+
+function App() {
+  const state = {
+    data: null,
+  };
+
+  const getData = async (city) => {
+    let response = await fetch(
+      `${API_URL}${city || 'moscow'}?unitGroup=us&key=${API_KEY}&contentType=json`,
+    );
+
+    if (response.ok) {
+      let json = await response.json();
+      return json;
+    }
+    return {};
+  };
+
+  const init = async () => {
+    const data = await getData();
+    state.data = { data };
+  };
+
+  const render = () => {};
+
+  return { init, render, getData };
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = App();
+  app.init();
+});
