d66687068cbf3933218f35966113d7bd2bf96a57
pav_nk <pav_nk@vk.com>
2025-02-25 23:11:21 +0300

    Updates

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = import.meta.env.VITE_API_KEY;
-
-const API_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`;
-
-export { API_KEY, API_URL };
