0afe749fbde71c276a42aa7a0ea4bbbedcc01acb
pav_nk <pav_nk@vk.com>
2025-02-25 19:33:58 +0300

    feat: the main functionality of the app is done

--- src/js/main.js
+++ src/js/main.js
@@ -11,14 +11,79 @@ import fogImg from '../images/fog.svg';
 import partlyCloudyDayImg from '../images/partly-cloudy-day.svg';
 import partlyCloudyNightImg from '../images/partly-cloudy-night.svg';
 
-const weatherImg = document.querySelector('#weather-img');
+const toggleCheckbox = document.getElementById('toggle-checkbox');
 const preloader = document.querySelector('#preloader');
 const searchForm = document.getElementById('search-form');
 const cardWrapper = document.getElementById('card-wrapper');
 
+const cardCity = document.getElementById('card-city');
+const cardTemp = document.getElementById('card-temp');
+const cardDesc = document.getElementById('card-description');
+const cardWindspeed = document.getElementById('card-windspeed');
+const cardLatitudeLongitude = document.getElementById(
+  'card-latitude-longitude',
+);
+const cardTimeZone = document.getElementById('card-timezone');
+const cardFeelslike = document.getElementById('card-feelslike');
+const weatherImg = document.querySelector('#weather-image');
+
+const weatherImages = {
+  wind: windImg,
+  rain: rainImg,
+  snow: snowImg,
+  'clear-day': clearDayImg,
+  'clear-night': clearNightImg,
+  cloudy: cloudyImg,
+  fog: fogImg,
+  'partly-cloudy-day': partlyCloudyDayImg,
+  'partly-cloudy-night': partlyCloudyNightImg,
+};
+
 function App() {
   const state = {
     data: null,
+    isCelsiusTemp: false,
+  };
+
+  const toggleCardView = () => {
+    preloader.classList.toggle('hidden');
+    cardWrapper.classList.toggle('hidden');
+  };
+
+  const getCurrentTemp = (temp) => {
+    return state.isCelsiusTemp
+      ? `${Math.floor(((temp - 32) * 5) / 9)} ℃`
+      : `${temp} F`;
+  };
+
+  const updateTempView = (temp, feelslike) => {
+    cardTemp.textContent = `${getCurrentTemp(temp)}`;
+    cardFeelslike.textContent = `${getCurrentTemp(feelslike)}`;
+  };
+
+  const updateView = () => {
+    toggleCardView();
+    const {
+      address,
+      description,
+      latitude,
+      longitude,
+      timezone,
+      icon,
+      temp,
+      windspeed,
+      feelslike,
+    } = state.data;
+
+    cardCity.textContent = `${address} `;
+    cardTemp.textContent = `${getCurrentTemp(temp)}`;
+    cardDesc.textContent = description;
+    cardWindspeed.textContent = `${windspeed} m / s`;
+    cardLatitudeLongitude.textContent = `${latitude} / ${longitude}`;
+    cardTimeZone.textContent = timezone;
+    cardFeelslike.textContent = `${getCurrentTemp(feelslike)}`;
+    weatherImg.src = weatherImages[icon];
+    weatherImg.setAttribute('alt', address);
   };
 
   const getData = async (city) => {
@@ -27,20 +92,47 @@ function App() {
     );
 
     if (response.ok) {
-      let json = await response.json();
-      return json;
+      toggleCardView();
+      const {
+        address,
+        description,
+        latitude,
+        longitude,
+        timezone,
+        currentConditions: { icon, temp, windspeed, feelslike },
+      } = await response.json();
+      state.data = {
+        address,
+        description,
+        latitude,
+        longitude,
+        timezone,
+        icon,
+        temp,
+        windspeed,
+        feelslike,
+      };
     }
-    return {};
   };
 
   const init = async () => {
-    const data = await getData();
-    state.data = { data };
+    await getData();
+    searchForm.addEventListener('submit', async (evt) => {
+      evt.preventDefault();
+      let formData = new FormData(searchForm);
+      await getData(formData.get('search'));
+      updateView();
+      searchForm.reset();
+    });
+    toggleCheckbox.addEventListener('change', () => {
+      state.isCelsiusTemp = !state.isCelsiusTemp;
+      updateTempView(state.data.temp, state.data.feelslike);
+    });
+    updateView();
+    toggleCardView();
   };
 
-  const render = () => {};
-
-  return { init, render, getData };
+  return { init };
 }
 
 document.addEventListener('DOMContentLoaded', () => {
